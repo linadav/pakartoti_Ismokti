37389d89c4a4ede9bdcd0bfff6d8aa1969a69eb3
linadav <lina_dav@yahoo.co.uk>
2019-12-07 10:36:27 +0200

    papildymas 2

--- src/carCar.java
+++ src/carCar.java
@@ -1,11 +1,22 @@
 public class carCar {
+    private String model;
+    private int count;
     private double price;
 
-    public carCar( double price) {
+    public carCar(String model, double price, int count) {
+        this.model = model;
         this.price = price;
+        this.count = count;
+    }
+
+    public String getModel() {
+        return model;
     }
 
     public double getPrice() {
         return price;
+    }
+    public int getCount() {
+        return count;
     }
 }
