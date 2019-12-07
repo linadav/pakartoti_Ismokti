35a4d2f87e2d077fcecf07eac306fa8154591b75
linadav <lina_dav@yahoo.co.uk>
2019-12-07 10:47:59 +0200

    pridetas kintamasis price

--- src/carCar.java
+++ src/carCar.java
@@ -1,12 +1,15 @@
 public class carCar {
+    double price;
     private int count;
 
     public carCar(String model, int count) {
-
+        // komentaras
         this.count = count;
     }
 
-
+    public double getPrice() {
+        return price;
+    }
 
     public int getCount() {
         return count;
