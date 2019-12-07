c53296c7b368b4049696abf8bce6b26916900505
linadav <lina_dav@yahoo.co.uk>
2019-12-07 10:45:20 +0200

    istrintas model kintamasis

--- src/carCar.java
+++ src/carCar.java
@@ -1,15 +1,12 @@
 public class carCar {
-    private String model;
     private int count;
 
     public carCar(String model, int count) {
-        this.model = model;
+
         this.count = count;
     }
 
-    public String getModel() {
-        return model;
-    }
+
 
     public int getCount() {
         return count;
